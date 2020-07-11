5e5c376c35c8176bc603d00179822897f732d432
unknown <mecaluxivansantobuono@gmail.com>
2020-07-11 01:39:38 -0300

    init vue proyect

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">    
+    <WeatherView />
+  </div>
+</template>
+
+<script>
+import WeatherView from './views/WeatherView.vue'
+
+export default {
+  name: 'App',
+  components: {
+    WeatherView
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
